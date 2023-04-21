6a209e703ac79b709343d35cf9d4dd5acc0e437f
Kei <opusk44@gmail.com>
2023-04-21 14:52:40 +0900

    add Get w/ RequestParam for test

--- src/main/java/com/example/tododemo/controller/TestController.java
+++ src/main/java/com/example/tododemo/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.tododemo.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,12 @@ public class TestController {
 
   @GetMapping("/{id}")
   public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
+    return "Hello World! ID " + id;
+  }
+
+  // host:port/test/testRequestParam?id=123
+  @GetMapping("/testRequestParam")
+  public String testControllerRequestParam(@RequestParam(required = false) int id) {
     return "Hello World! ID " + id;
   }
 
