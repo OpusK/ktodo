f6fe0b0c596f20c537e21075ef3d4d9c153ef833
Kei <opusk44@gmail.com>
2023-04-21 14:46:59 +0900

    add Get with Parameter for test

--- src/main/java/com/example/tododemo/controller/TestController.java
+++ src/main/java/com/example/tododemo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.tododemo.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +14,10 @@ public class TestController {
     System.out.println("Testing");
     return "Hello World!";
   }
-  
+
+  @GetMapping("/{id}")
+  public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
+    return "Hello World! ID " + id;
+  }
+
 }
